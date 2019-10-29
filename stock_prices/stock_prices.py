77c472093a929d0cf1f45ee9b5ca22ddef5b7720
Angelo Deleon <rudy209@comcast.net>
2019-10-28 22:04:12 -0700

    updated stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python
-
+import time
 import argparse
 
+start_time = time.time()
+
 def find_max_profit(prices):
-  pass
+  buy = prices[0]
+  sell = 0
+
+  for price in prices[1:]:
+        
+
+  max_profit = buy - sell
+return max_profit
+
+
+Day1 = [10, 7, 5, 8, 11, 9] # / 6
+Day2 = [100, 90, 80, 50, 20, 10] # / 10
+find_max_profit(Day1)
+find_max_profit(Day2)
+
+# * Prints execution time to console
+print("--- %s seconds ---" % (time.time() - start_time))
 
 
 if __name__ == '__main__':
